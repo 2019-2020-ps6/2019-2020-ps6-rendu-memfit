54eaeca6b13cfb3c3ae99ee1c5ec8b19a3ff8390
Alexandre-MAZURIER <a.mazurier83@gmail.com>
2020-03-17 11:15:56 +0100

    Quiz creation #7

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { QuizComponent } from './quizzes/quiz/quiz.component';
 import { HeaderComponent } from './header/header.component';
-import { QuizFormComponent } from './quizzes/quiz-form/quiz-form.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { AppRoutingModule } from './app.routing.module';
 import { QuestionListComponent } from './questions/question-list/question-list.component';
@@ -15,6 +14,7 @@ import { QuestionComponent } from './questions/question/question.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MenuComponent } from './index/menu/menu.component';
 import { IndexComponent } from './index/index.component';
+import { CreateQuizComponent } from './quizzes/create-quiz/create-quiz.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +22,13 @@ import { IndexComponent } from './index/index.component';
     QuizListComponent,
     QuizComponent,
     HeaderComponent,
-    QuizFormComponent,
     EditQuizComponent,
     QuestionListComponent,
     QuestionFormComponent,
     QuestionComponent,
     MenuComponent,
-    IndexComponent
+    IndexComponent,
+    CreateQuizComponent
   ],
   imports: [
     BrowserModule,
