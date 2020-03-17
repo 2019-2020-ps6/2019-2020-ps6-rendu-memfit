177b8ea50ce0bfae3bce24895c53b698760527e8
Alexandre-MAZURIER <a.mazurier83@gmail.com>
2020-03-17 10:06:46 +0100

    Index component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { QuestionListComponent } from './questions/question-list/question-list.c
 import { QuestionFormComponent } from './questions/question-form/question-form.component';
 import { QuestionComponent } from './questions/question/question.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './index/menu/menu.component';
 import { IndexComponent } from './index/index.component';
 
 @NgModule({
