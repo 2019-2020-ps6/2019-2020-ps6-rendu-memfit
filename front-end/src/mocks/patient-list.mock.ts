daa50041290612c2af607455d29d0e6927852335
Anthony Barna <anthony.barna@etu.univ-cotedazur.fr>
2020-03-24 12:04:34 +0100

    frontend de la visualisation des patients #6 #8

---
+++ front-end/src/mocks/patient-list.mock.ts
@@ -0,0 +1,14 @@
+import {Patient} from "../models/patient.model";
+
+export const PATIENT_LIST: Patient[] = [
+  {
+    name: 'Jack Joe',
+    id: 0,
+    photo: "https://pbs.twimg.com/profile_images/1113424741237444608/usxnv-DD_400x400.jpg",
+  },
+  {
+    name: 'Foo Bar',
+    id: 1,
+    photo: "",
+  }
+];
