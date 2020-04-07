b7264510664825f669a70d3d95a7b38c769b63fd
Antoine FACQ <antoine.facq@gmail.com>
2020-04-07 08:37:32 +0200

    Removed patientId (not needed)

--- back-end/app/models/patient.model.js
+++ back-end/app/models/patient.model.js
@@ -2,7 +2,6 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Patient', {
-  patientId: Joi.number().required(),
   firstName: Joi.string(),
   lastName: Joi.string(),
   quizIds: Joi.array().required(),
