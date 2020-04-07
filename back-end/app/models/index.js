eb11022a49fcf925efc42405a95e978267a846bd
Antoine FACQ <antoine.facq@gmail.com>
2020-04-07 12:42:37 +0200

    Added Backend Models for QuizRecord

--- back-end/app/models/index.js
+++ back-end/app/models/index.js
@@ -3,9 +3,14 @@ const Question = require('./question.model.js')
 const Quiz = require('./quiz.model.js')
 const User = require('./patient.model.js')
 
+const QuizRecord = require('./quizrecord.model.js')
+const AnswerRecord = require('./answerrecord.model.js')
+
 module.exports = {
   Answer,
   Question,
   Quiz,
   User,
+  QuizRecord,
+  AnswerRecord
 }
