ba2a9b30d3ad8cb4da220bbc35d5dd899d6994fe
Antoine FACQ <antoine.facq@gmail.com>
2020-04-21 14:18:24 +0200

    Updated Quiz Layout

--- back-end/app/api/index.js
+++ back-end/app/api/index.js
@@ -1,11 +1,52 @@
-const { Router } = require('express')
+const {Router} = require('express')
 const QuizzesRouter = require('./quizzes')
 const QuizRecordRouter = require('./quizrecord')
 const PatientRouter = require('./patients')
 
+var path = require('path');
+
+const fs = require("fs");
+const multipart = require('connect-multiparty');
+
+var EXT_RE = /(\.[_\-a-zA-Z0-9]{0,16}).*/g;
+
+const multipartMiddleware = multipart({
+  uploadDir: './uploads'
+});
+
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
-// router.get('/quizzes/quizRecord/', (req, res) => res.status(200).json('ok'))
+
+router.get('/uploads/:fileName', (req, res) => {
+
+  var file = path.resolve(__dirname+'/../../uploads/'+req.params.fileName)
+  if (fs.existsSync(file)) {
+    res.sendFile(file);
+  }else {
+    res.status(404)
+  }
+})
+
+router.post('/upload', multipartMiddleware, (req, res) => {
+  console.log()
+  if(req.files.uploads[0] !== undefined) {
+    var file = req.files.uploads[0].path;
+    var newFile = file.split("/").pop();
+    console.log(newFile)
+    // console.log(req.files.upload.path);
+    // console.log(file.type);
+    // fs.rename(req.files[0].originalFilename, file, function (err) {
+    //   if (err) {
+    //     console.log(err);
+    //   }
+    //
+    // })
+
+    res.json({
+      'filepath': file
+    });
+  }
+});
 router.use('/quizzes', QuizzesRouter)
 router.use('/patients', PatientRouter)
 router.use('/quizRecord', QuizRecordRouter)
--- front-end/src/app/quizzes/quiz-show/quiz-show.component.ts
+++ front-end/src/app/quizzes/quiz-show/quiz-show.component.ts
@@ -1,10 +1,11 @@
-import {Component, ComponentFactoryResolver, Input, OnInit, OnDestroy} from '@angular/core';
+import {Component, ComponentFactoryResolver, Input, OnInit, OnDestroy, ElementRef} from '@angular/core';
 import {Quiz} from "../../../models/quiz.model";
 import {Question} from "../../../models/question.model";
 import {QuizRecord, AnswerRecord} from "../../../models/quizrecord.model";
 import {QuizService} from "../../../services/quiz.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {QUESTION_ACTOR, QUESTION_ACTORS, QUIZ_LIST} from "../../../mocks/quiz-list.mock";
+import {ImageService} from "../../../services/image.service";
 
 @Component({
   selector: 'app-quiz-show',
@@ -28,7 +29,7 @@ export class QuizShowComponent implements OnInit {
   quizRecord: QuizRecord;
   answerRecord: AnswerRecord = null;
 
-  constructor(private route: ActivatedRoute, private quizService: QuizService) {
+  constructor(private route: ActivatedRoute, private quizService: QuizService, private imgService: ImageService) {
     this.quizService.quizSelected$.subscribe((quiz) => this.quizInit(quiz));
   }
 
