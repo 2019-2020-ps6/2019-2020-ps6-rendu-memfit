9afee4c830d5c607b017d870bbc7f180be867d39
Hadrien Bonato-Pape <hadrien.bonato--pape@etu.unice.fr>
2020-04-25 04:12:33 +0200

    Update quiz settings IHM

--- front-end/src/app/quizzes/edit-quiz/update-quiz-settings/update-quiz-settings.component.ts
+++ front-end/src/app/quizzes/edit-quiz/update-quiz-settings/update-quiz-settings.component.ts
@@ -1,5 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Quiz} from '../../../../models/quiz.model';
+import {Patient} from '../../../../models/patient.model';
+import {ImageChoicePopupComponent} from '../../../image-choice-popup/image-choice-popup.component';
+import {FormBuilder} from '@angular/forms';
+import {QuizService} from '../../../../services/quiz.service';
+import {PatientService} from '../../../../services/patient.service';
+import {Router} from '@angular/router';
+import {MatDialog} from '@angular/material';
 
 @Component({
   selector: 'app-update-quiz-settings',
@@ -11,9 +18,67 @@ export class UpdateQuizSettingsComponent implements OnInit {
   @Input()
   quiz: Quiz;
 
-  constructor() { }
+  public photoURL: string;
+  public updateQuizForm: any;
+  public patientList: Patient[] = [];
+  patientId: number;
+  selectedP: number;
+
+  constructor(public formBuilder: FormBuilder, public quizService: QuizService, public patientService: PatientService, private dialogPhoto: MatDialog)
+  {
+    this.photoURL = "assets/quiz-logo.png";
+    this.updateQuizForm = this.formBuilder.group({
+      name: [''],
+      theme: ['']
+    });
+    this.patientService.patients$.subscribe((patients: Patient[]) => {
+      this.patientList = patients;
+    });
+  }
 
   ngOnInit() {
   }
 
+  addQuiz() {
+    const quizToCreate: Quiz = this.updateQuizForm.getRawValue() as Quiz;
+    const dateNow = Date.now();
+    quizToCreate.id = dateNow;
+    quizToCreate.image = this.photoURL;
+    this.quizService.addQuiz(quizToCreate);
+    this.patientService.addQuizToPatient(dateNow, this.patientService.getPatient(this.selectedP));
+  }
+
+  selectPatientSup() {
+
+  }
+
+  syncImg(value: string) {
+    if(value == "") {
+      this.photoURL = "assets/quiz-logo.png";
+    }
+    else this.photoURL = value;
+  }
+
+
+  selected(e) {
+    this.selectedP = e.target.value.split(" ")[1];
+    console.warn(this.selectedP)
+  }
+
+  onUpload(uploadedFile: string) {
+    this.photoURL = "http://localhost:9428/api/" + uploadedFile;
+  }
+
+  openDialogPhoto(): void {
+    const dialogRef = this.dialogPhoto.open(ImageChoicePopupComponent, {
+      width: '600px',
+      data: {profileImgURL: this.photoURL}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result != null) {
+        this.photoURL = result;
+      }
+    });
+  }
 }
